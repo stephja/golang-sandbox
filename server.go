911a0d79409aa2c1c64f4346e1efd6f47aa57897
Stephen Jacobs <stephenjay1187@gmail.com>
2018-12-30 15:50:46 -0800

    Added index.html file
    Added index.html file and codes in server.go to serve static files.

--- server.go
+++ server.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-    
-    http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "users")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Fprintf(w, "index", html.EscapeString(r.URL.Path))
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 
-    log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "users")
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 
 }
